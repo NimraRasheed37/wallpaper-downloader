d23ff852eff4af8551c47d7f9cbd5e5d919d0a65
Nimra Rasheed <60661441+NimraRasheed37@users.noreply.github.com>
2024-09-22 14:43:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ images.forEach(image => {
     image.addEventListener('click', () => {
         fullscreenImg.src = image.src;
         downloadBtn.href = image.src; // Set the download link to the image source
-        //overlay.style.display = 'flex';
-        fullscreenOverlay.style.display = 'block';
+        overlay.style.display = 'flex';
+       // fullscreenOverlay.style.display = 'block';
     });
 });
 
